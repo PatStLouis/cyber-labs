265540584e0148644132e9928d222ea7b7195989
PatStLouis <43082425+PatStLouis@users.noreply.github.com>
2021-05-31 11:22:14 -0400

    Update client-setup.sh

--- client-setup.sh
+++ client-setup.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get install -y \
-	nmap
-	snapd
-	
-sudo systemctl enable snapd
-sudo systemctl start snapd
-sudo snap install john-the-ripper
+sudo apt-get install -y nmap
