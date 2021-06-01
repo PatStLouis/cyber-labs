206b43687022c8abe58a6780e34557a2061680ec
PatStLouis <43082425+PatStLouis@users.noreply.github.com>
2021-06-01 04:17:12 -0400

    Update client-setup.sh

--- client-setup.sh
+++ client-setup.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-sudo apt-get install -y nmap
+sudo apt-get install -y \
+  snapd \
+  nmap \
+  hydra \
+  steghide \
+  binwalk \
+  exiftool \
+  hexedit \
+  curl \
+  netcat \
+  python3-dev \
+  tmux \
+  tcpdump;
+  
+sudo snap install \
+  john-the-ripper
+  
+sudo apt install -y libffi-dev
+sudo pip3 install sqlmap requests
