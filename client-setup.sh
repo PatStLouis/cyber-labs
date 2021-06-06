08e993811d80cefdab3162fa9b072a504c4c7c34
PatStLouis <43082425+PatStLouis@users.noreply.github.com>
2021-06-06 14:52:22 -0400

    Update client-setup.sh

--- client-setup.sh
+++ client-setup.sh
@@ -3,34 +3,34 @@ sudo mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
 
 #!/bin/bash
 sudo apt-get install -y \
-  snapd \
-  git \
-  xrdp \
-  openvpn \
-  docker.io \
-  docker-compose \
-  neovim \
-  code \
-  unzip \
-  zip \
-  curl \
-  netcat \
-  python3 \
-  python3-dev \
-  python3-pip \
-  terminator \
-  tmux \
-  whois \
-  rsync \
-  gimp \
-  nmap \
-  hydra \
-  steghide \
-  gobuster \
-  binwalk \
-  exiftool \
-  hexedit \
-  tcpdump
+snapd \
+git \
+xrdp \
+openvpn \
+docker.io \
+docker-compose \
+neovim \
+code \
+unzip \
+zip \
+curl \
+netcat \
+python3 \
+python3-dev \
+python3-pip \
+terminator \
+tmux \
+whois \
+rsync \
+gimp \
+nmap \
+hydra \
+steghide \
+gobuster \
+binwalk \
+exiftool \
+hexedit \
+tcpdump
   
 sudo snap install \
   john-the-ripper
