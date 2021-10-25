75d21bba8d064ebe1dd1899b2620cc3827e69f91
PatStLouis <43082425+PatStLouis@users.noreply.github.com>
2021-10-25 08:55:39 -0700

    Update client.sh

--- client.sh
+++ client.sh
@@ -9,6 +9,3 @@ sudo mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
 sudo apt install curl -y
 # Base tools
 curl -s https://raw.githubusercontent.com/PatStLouis/cyber-labs/main/tools | bash
-# Dataplicity client
-curl -s https://raw.githubusercontent.com/PatStLouis/cyber-labs/main/dataplicity | bash
-
