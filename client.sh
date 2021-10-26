3f89d40800cb5b6e25bcc6f0ed6eb000cb44f30a
PatStLouis <43082425+PatStLouis@users.noreply.github.com>
2021-10-25 18:30:36 -0700

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+apt update -y && apt upgrade -y
+apt install -y curl
 
-sudo apt update -y && sudo apt upgrade -y
+# Docker
+apt install -y apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+sudo usermod -aG docker dietpi
 
 # diet pi lxpolkit error hack
-sudo mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
+# sudo mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
 
-sudo apt install curl -y
-# Base tools
+# Hacking tools
 curl -s https://raw.githubusercontent.com/PatStLouis/cyber-labs/main/tools | bash
