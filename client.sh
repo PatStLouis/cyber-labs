3cf39d4c9db3dee7369e1c41d9a383f46466722d
PatStLouis <43082425+PatStLouis@users.noreply.github.com>
2021-10-26 16:15:50 -0700

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
-
-apt update -y && apt upgrade -y
-apt install -y curl
+apt update && sudo apt upgrade -y
+apt-get install -y \
+git \
+curl \
+snapd \
+unzip \
+zip \
+netcat \
+python3 \
+python3-dev \
+python3-pip \
+terminator \
+tmux \
+whois \
+rsync \
+nmap \
+hydra \
+steghide \
+gobuster \
+binwalk \
+exiftool \
+hexedit \
+tcpdump;
 
 # Docker
 apt install -y apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
-sudo usermod -aG docker dietpi
 
-# diet pi lxpolkit error hack
+# Juice-shop
+docker run -d --name juice-shop -p 3000:3000 santosomar/juice-shop-arm64
+
+snap install john-the-ripper
+snap install cool-retro-term --classic
+
+echo "alias john='snap run john-the-ripper'" >> ~/.bashrc
+echo "alias cool='snap run cool-retro-term'" >> ~/.bashrc
+
+pip3 install \
+sqlmap \
+requests \
+pynvim
+
+# diet pi lxpolkit error fix/hack, keep for reference do not comment out
 # sudo mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
-
-# Hacking tools
-curl -s https://raw.githubusercontent.com/PatStLouis/cyber-labs/main/tools | bash
